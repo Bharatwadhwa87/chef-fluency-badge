c3a47f908539fbceb4d73b74382f3402d727f10f
Bharat <bharatwadhwa87@gmail.com>
2018-04-25 09:00:49 +0000

    Added the motd file

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,9 +4,16 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
+#
+
+if node['platform_family'] == "rhel"
+  package 'httpd'
+elsif node['platform_family'] == "debian"
+  package 'apache2'
+end
  
-Package 'apache2' do
-      package_name 'httpd'
+package 'apache2' do
+      package_name package
       action :install
 end 
 
@@ -14,3 +21,4 @@ service 'apache2' do
       service_name 'httpd'
       action [:start,:enable]
 end
+
